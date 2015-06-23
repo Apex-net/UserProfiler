12f1721b2c942fc55fb8b4e3ebaac74ded05ae7e
mpaciApex <m.paci@apexnet.it>
2015-06-23 11:04:59 +0200

    sistemata stringa di connessione

--- dev/UserProfiler.Model/UserProfiler.Designer.cs
+++ dev/UserProfiler.Model/UserProfiler.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\work\UserProfiler\Sviluppo\dev\UserProfiler.Model\UserProfiler.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\work\UserProfiler\dev\UserProfiler.Model\UserProfiler.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
