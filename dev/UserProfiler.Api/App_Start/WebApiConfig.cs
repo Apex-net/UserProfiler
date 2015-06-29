85a3d6d80b4efdd43df2fdbbc647dab809d24ee7
Ali Servet Donmez <asd@pittle.org>
2015-06-29 11:51:17 +0200

    Sistemato warning segnalati da StyleCop

--- dev/UserProfiler.Api/App_Start/WebApiConfig.cs
+++ dev/UserProfiler.Api/App_Start/WebApiConfig.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
-
-namespace UserProfiler.Api
+﻿namespace UserProfiler.Api
 {
+    using System.Web.Http;
+
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -14,11 +11,7 @@ namespace UserProfiler.Api
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
         }
     }
 }
