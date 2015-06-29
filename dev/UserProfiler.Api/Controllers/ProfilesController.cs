ded394cbe187a2e38df52d2dd1c2a17962d28157
Ali Servet Donmez <asd@pittle.org>
2015-06-29 12:55:47 +0200

    Aggiunto boilerplate ProfilesController

---
+++ dev/UserProfiler.Api/Controllers/ProfilesController.cs
@@ -0,0 +1,18 @@
+﻿namespace UserProfiler.Api.Controllers
+{
+    using System.Collections.Generic;
+    using Common.Controllers.Api;
+
+    public class ProfilesController : BaseApiController
+    {
+        // GET: api/Profiles
+        public IEnumerable<string> Get(string userId)
+        {
+            return new[]
+            {
+                "value1", 
+                "value2"
+            };
+        }
+    }
+}
